538e3c31c257874fa315a7f81c892916f45ae6af
kunal-copods <kunal@copods.co>
2023-02-16 21:02:05 +0530

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,46 @@
 //@ts-ignore
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 //@ts-ignore
 import useStore from "container/store";
 
-
 //@ts-ignore
-const CounterAppOne = React.lazy(() => import('remote/CounterOne'));
+const CounterAppOne = React.lazy(() => import("remote/CounterOne"));
 //@ts-ignore
-const Pricing = React.lazy(() => import('pricing/Pricing'));
+const Pricing = React.lazy(() => import("pricing/Pricing"));
 
 function App() {
   //@ts-ignore
   const { isLoggedIn, login, logout, user } = useStore();
   return (
-    <div className="App bg-gray-200 h-screen flex flex-col">
-      <div className='flex justify-between px-5 py-4'>
-        <header className='flex gap-2'>
-          {/* <img src='' alt='logo' width={24} height={24} /> */}
-          <h2 className='text-base text-gray-800 font-bold'>Stealth-SaaS</h2>
-        </header>
-        {isLoggedIn ? <button className=' ' onClick={logout}>Logout</button> : <button className='' onClick={login}>login</button>}
+    <div className=" bg-black h-screen flex flex-col">
+      <div className="bg-gray-700 flex items-center justify-between px-5 py-4">
+        <div className="text-base text-gray-300 font-bold">Stealth-SaaS</div>
+
+        <div>
+          {isLoggedIn ? (
+            <button className="text-red-500" onClick={logout}>
+              Logout
+            </button>
+          ) : (
+            <button className="text-gray-300" onClick={login}>
+              Login
+            </button>
+          )}
+        </div>
       </div>
-      <div className='bg-gray-400 h-full flex items-center justify-center gap-4'>
+      <div className="bg-black text-gray-200 h-full flex items-center justify-center gap-4">
         {!isLoggedIn && <h1>Welcome to Stealth SaaS! Please Login</h1>}
         <Suspense fallback={<h1>Signing In</h1>}>
-          {isLoggedIn && <div className='flex flex-col'>
-            <h1>Welcome, {user.name}</h1>
-            <div className='flex gap-4'>
-
-              <CounterAppOne />
-              <Pricing />
+          {isLoggedIn && (
+            <div className="flex flex-col gap-4">
+              <h1>Welcome, {user.name}</h1>
+              <div className="flex gap-4">
+                <CounterAppOne />
+                <Pricing />
+              </div>
             </div>
-          </div>
-          }
+          )}
         </Suspense>
-
-
       </div>
       {/* <h1>{count}</h1>
       <button onClick={increment}>+</button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,14 @@
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#1B73E8',
-            },
-        },
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1B73E8",
+      },
     },
-    plugins: [],corePlugins: {
-        preflight: false,
-    }
+  },
+  plugins: [],
+  // corePlugins: {
+  //   preflight: false,
+  // },
 };
